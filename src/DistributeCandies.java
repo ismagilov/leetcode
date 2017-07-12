4f2b31b011ee0bc4953a4b20d762c6a193020f33
Ilya Ismagilov <ismagilov@gmail.com>
2017-07-12 14:57:27 +0700

    Sovled

---
+++ src/DistributeCandies.java
@@ -0,0 +1,13 @@
+/**
+* https://leetcode.com/articles/distribute-candies/
+*/
+public class DistributeCandies {
+    public int distributeCandies(int[] candies) {
+        Set<Integer> kinds = new HashSet<>();
+        
+        for (int c : candies)
+            kinds.add(c);
+        
+        return Math.min(kinds.size(), candies.length / 2);
+    }
+}
