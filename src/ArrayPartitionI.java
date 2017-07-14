9af5766aa3ddb5d8ba6aa72f46652b2780c5af4e
Ilya Ismagilov <ismagilov@gmail.com>
2017-07-14 11:37:25 +0700

    Solved using counting sort

---
+++ src/ArrayPartitionI.java
@@ -0,0 +1,25 @@
+/**
+* https://leetcode.com/problems/array-partition-i
+*/
+public class ArrayPartitionI {
+    public int arrayPairSum(int[] nums) {
+        int[] sorted = new int [20001];
+        
+        for (int i = 0; i < nums.length; i++)
+            sorted[nums[i] + 1000]++;
+        
+        int sum = 0;
+        boolean odd = true;
+        for (int i = 0; i < sorted.length; i++) {
+            while (sorted[i] > 0) {
+                if (odd)
+                    sum += i - 1000;
+                    
+                odd = !odd;
+                sorted[i]--;
+            }
+        }
+        
+        return sum;
+    }
+}
