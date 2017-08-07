808d96826a8d8490eaab0c79392a42eb898a1db1
Ilya Ismagilov <ismagilov@gmail.com>
2017-08-07 14:37:36 +0700

    Solved

---
+++ src/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,15 @@
+import lib.TreeNode;
+
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree
+ */
+public class LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val)
+            return lowestCommonAncestor(root.left, p, q);
+        if (p.val > root.val && q.val > root.val)
+            return lowestCommonAncestor(root.right, p, q);
+        else
+            return root;
+    }
+}
