d12e89ed6014b955f922174e2ee76bf31b5e4c17
Ilya Ismagilov <ismagilov@gmail.com>
2017-08-10 13:58:17 +0700

    Solved

---
+++ src/UglyNumber.java
@@ -0,0 +1,27 @@
+/**
+ * https://leetcode.com/problems/ugly-number/description/
+ */
+public class UglyNumber {
+    public boolean isUgly(int num) {
+        if (num <= 0)
+            return false;
+
+        while (num % 2 == 0)
+            num /= 2;
+
+        while (num % 3 == 0)
+            num /= 3;
+
+        while (num % 5 == 0)
+            num /= 5;
+
+        return num == 1;
+    }
+
+    public static void main(String[] args) {
+        UglyNumber un = new UglyNumber();
+
+        System.out.println(un.isUgly(60));
+    }
+
+}
