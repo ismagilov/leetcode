64140245f5bb9c48ef359f6e978c4ec69e7b3bb5
Ilya Ismagilov <ismagilov@gmail.com>
2017-08-11 18:06:04 +0700

    Solved word pattern

---
+++ src/WordPattern.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+
+/**
+ * https://leetcode.com/problems/word-pattern
+ */
+public class WordPattern {
+    public boolean wordPattern(String pattern, String str) {
+        HashMap<Character, String> ps = new HashMap<>();
+
+        String[] words = str.split(" ");
+
+        if (pattern.length() != words.length)
+            return false;
+
+        for (int i = 0; i < words.length; i++) {
+            Character p = pattern.charAt(i);
+
+            if (ps.containsKey(p)) {
+                if (!ps.get(p).equals(words[i]))
+                    return false;
+            } else {
+                if (ps.containsValue(words[i]))
+                    return false;
+
+                ps.put(p, words[i]);
+            }
+        }
+
+        return true;
+    }
+}
