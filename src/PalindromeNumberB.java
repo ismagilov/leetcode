7ccfdaa9e107f509ec126cf200d085f2d2b5fffc
Ilya Ismagilov <ismagilov@gmail.com>
2017-08-15 15:25:57 +0700

    Solve palindrome number

---
+++ src/PalindromeNumberB.java
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/palindrome-number
+ *
+ * Solve using composing new number from the last digit of input number and comparing them, for 46764: 46 vs 467
+ */
+public class PalindromeNumberB {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x != 0 && x % 10 == 0))
+            return false;
+
+        int rev = 0;
+        while (x > rev) {
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+
+        return x == rev || x == rev / 10;
+    }
+
+    public static void main(String[] args) {
+        PalindromeNumberB p = new PalindromeNumberB();
+
+        System.out.println(p.isPalindrome(212));
+    }
+}
