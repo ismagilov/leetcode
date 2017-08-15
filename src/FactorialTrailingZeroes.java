747ce5d53647b5f528c825cd089480e0344cd3f5
Ilya Ismagilov <ismagilov@gmail.com>
2017-08-15 15:24:36 +0700

    Solve factorial trailing zeros

---
+++ src/FactorialTrailingZeroes.java
@@ -0,0 +1,13 @@
+/**
+ * https://leetcode.com/problems/factorial-trailing-zeroes
+ */
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int r = 0;
+        while (n > 0) {
+            n /= 5;
+            r += n;
+        }
+        return r;
+    }
+}
