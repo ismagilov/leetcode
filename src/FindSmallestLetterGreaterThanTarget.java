e21dd56f229aaf5716a8b5629b0502ee5d927fa0
Ilya Ismagilov <ismagilov@gmail.com>
2018-01-05 18:33:45 +0700

    Simplified binary search

--- src/FindSmallestLetterGreaterThanTarget.java
+++ src/FindSmallestLetterGreaterThanTarget.java
@@ -3,16 +3,15 @@
 */
 public class FindSmallestLetterGreaterThanTarget {
     public char nextGreatestLetter(char[] letters, char target) {
-        int b = 0, e = letters.length;
+        int b = 0, e = letters.length - 1;
         
-        while (b < e) {
+        while (b <= e) {
             int mid = b + (e - b) / 2;
             
-            if (letters[mid] <= target) {
+            if (letters[mid] <= target)
                 b = mid + 1;
-            } else {
-                e = mid;
-            }
+            else
+                e = mid - 1;
         }
         
         return letters[b % letters.length]; 
