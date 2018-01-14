8f10f74cc7ce3abf2bdc3e6516f5ae253eefe2e4
Ilya Ismagilov <ismagilov@gmail.com>
2018-01-14 11:46:40 +0700

    Solved PartitionLabels

---
+++ src/PartitionLabels.java
@@ -0,0 +1,33 @@
+/**
+* https://leetcode.com/contest/weekly-contest-67/problems/partition-labels/
+*/
+public class PartitionLabels {
+    public List<Integer> partitionLabels(String S) {
+        List<Integer> res = new ArrayList<>();
+
+        int[] maxPos = new int[26];
+        char[] chs = S.toCharArray();
+        for (int i = 0; i < chs.length; i++) {
+            int idx = chs[i] - 'a';
+            maxPos[idx] = i;
+        }
+        
+        int right = -1;
+        int left = -1;
+        for (int i = 0; i < chs.length; i++) {
+            int idx = chs[i] - 'a';
+            
+            if (left == -1)
+                left = i;
+            
+            right = Math.max(right, maxPos[idx]);
+            
+            if (i == right) {
+                res.add(right - left + 1);
+                left = -1;
+            }
+        }
+        
+        return res;
+    }
+}
