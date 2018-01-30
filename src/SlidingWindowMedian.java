33b46c1145fd49f73b5225617317123a0f8fd847
Ilya Ismagilov <ismagilov@gmail.com>
2018-01-30 18:44:00 +0700

    Solved SlidingWindowMedian

---
+++ src/SlidingWindowMedian.java
@@ -0,0 +1,50 @@
+/**
+* https://leetcode.com/problems/sliding-window-median/description/
+*/
+public class SlidingWindowMedian {
+    private PriorityQueue<Integer> left = new PriorityQueue<>(Comparator.reverseOrder());
+    private PriorityQueue<Integer> right = new PriorityQueue<>();
+
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        double[] medians = new double[nums.length - k + 1];
+               
+        for (int i = 0; i < k; i++)
+            add(nums[i]);
+                        
+        int idx = 0;
+        medians[idx++] = median();
+        for (int i = 0; i < nums.length - k; i++) {
+            remove(nums[i]);
+            add(nums[i + k]);
+            
+            medians[idx++] = median();
+        }
+        
+        return medians;
+    }
+    
+    private void add(int n) {
+        left.offer(n);
+        
+        right.offer(left.poll());
+        
+        if (right.size() > left.size())
+            left.offer(right.poll());
+    }
+    
+    private void remove(int n) {
+        if (n <= median())
+            left.remove(n);
+        else 
+            right.remove(n);
+        
+        if (right.size() > left.size())
+            left.offer(right.poll());
+        else if (left.size() - right.size() > 1)
+            right.offer(left.poll());
+    }
+    
+    private double median() {
+        return left.size() == right.size() ? ((double)left.peek() + (double)right.peek()) / 2.0: left.peek();
+    }
+}
