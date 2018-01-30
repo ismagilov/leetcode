e1b2f31bdd16a0f05de35ee0f03dbfeb8f5fc80d
Ilya Ismagilov <ismagilov@gmail.com>
2018-01-30 19:09:22 +0700

    Sovled SlidingWindowMedianB, using sorted array

---
+++ src/SlidingWindowMedianB.java
@@ -0,0 +1,46 @@
+/**
+* https://leetcode.com/problems/sliding-window-median/description/
+*
+* Use sorted array for window.
+*/
+public class SlidingWindowMedianB {
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        double[] medians = new double[nums.length - k + 1];
+        ArrayList<Integer> w = new ArrayList<>();
+        
+        for (int i = 0; i < k; i++)
+            w.add(nums[i]);
+        
+        w.sort(Comparator.naturalOrder());
+        
+        int idx = 0;
+        medians[idx++] = median(w);
+        for (int i = 0; i < nums.length - k; i++) {
+            remove(w, nums[i]);
+            add(w, nums[i + k]);
+            
+            medians[idx++] = median(w);
+        }
+        
+        return medians;
+    }
+    
+    private void add(ArrayList<Integer> w, int n) {
+        int idx = 0;
+        
+        while (idx < w.size() && n > w.get(idx))
+            idx++;
+        
+        w.add(idx, n);
+    }
+    
+    private void remove(ArrayList<Integer> w, int n) {
+        w.remove(Integer.valueOf(n));
+    }
+    
+    private double median(ArrayList<Integer> w) {
+        int sz = w.size();
+        
+        return sz % 2 == 0 ? ((double)w.get(sz / 2) + (double)w.get(sz / 2 - 1)) / 2.0 : w.get(sz / 2);
+    }
+}
