dfd7cabbc3aa6b6c79e89435730e65c8ef7c377f
Ilya Ismagilov <ismagilov@gmail.com>
2018-02-01 14:46:53 +0700

    Solved OddEvenLinkedListB

---
+++ src/OddEvenLinkedListB.java
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/problems/odd-even-linked-list/description/
+ *
+ * Support two lists: head and odd tail, evenHead and even tail.
+ *
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class OddEvenLinkedListB {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        
+        ListNode odd = head, evenHead = head.next, even = evenHead;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        
+        odd.next = evenHead;
+        
+        return head;
+    }
+}
