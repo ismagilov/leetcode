8443469c2acb69c6c34fafc34ae3e679c39fa1d8
Ilya Ismagilov <ismagilov@gmail.com>
2018-02-13 17:47:28 +0700

    Solved DesignCompressedStringIterator

---
+++ src/DesignCompressedStringIterator.java
@@ -0,0 +1,45 @@
+/**
+* https://leetcode.com/problems/design-compressed-string-iterator/description/
+*/
+public class DesignCompressedStringIterator {
+    private final char[] compressed;
+    private int p;
+    
+    private char ch;
+    private int cnt;
+    
+    public StringIterator(String compressedString) {
+        compressed = compressedString.toCharArray();
+        p = 0;
+    }
+    
+    public char next() {
+        if (cnt == 0 && p == compressed.length)
+            return ' ';
+        else if (cnt == 0 && p < compressed.length) {
+            ch = compressed[p];
+            p++;
+
+            StringBuilder sb = new StringBuilder();
+            while (p < compressed.length && Character.isDigit(compressed[p])) {
+                sb.append(compressed[p]);
+                p++;
+            }
+            cnt = Integer.valueOf(sb.toString());
+        }
+        
+        cnt--;
+        return ch;
+    }
+    
+    public boolean hasNext() {
+        return cnt > 0 || p != compressed.length;
+    }
+}
+
+/**
+ * Your StringIterator object will be instantiated and called as such:
+ * StringIterator obj = new StringIterator(compressedString);
+ * char param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
