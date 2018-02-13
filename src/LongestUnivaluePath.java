2154c4473d4532aa595a98cf733d1e0c3a7081b4
Ilya Ismagilov <ismagilov@gmail.com>
2018-02-13 17:48:20 +0700

    Solved LongestUnivaluePath

---
+++ src/LongestUnivaluePath.java
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/longest-univalue-path/description/
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class LongestUnivaluePath {
+    private int res = 0;
+    
+    public int longestUnivaluePath(TreeNode root) {
+        
+        longest(root);
+        
+        return res;
+    }
+    
+    private int longest(TreeNode root) {
+        if (root == null) return 0;
+    
+        int left = longest(root.left);
+        int right = longest(root.right);
+        
+        int pathLeft = 0, pathRight = 0;
+        if (root.left != null && root.val == root.left.val)
+            pathLeft = left + 1;
+        if (root.right != null && root.val == root.right.val)
+            pathRight = right + 1;
+        
+        res = Math.max(res, pathLeft + pathRight);
+        
+        return Math.max(pathLeft, pathRight);
+    }
+}
