47e1e7537307327a900f636210c7aa8d0a3e4f77
Ilya Ismagilov <ismagilov@gmail.com>
2018-02-15 18:43:11 +0700

    Solved ClosestBinarySearchTreeValue

---
+++ src/ClosestBinarySearchTreeValue.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/closest-binary-search-tree-value/description/
+ *
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class ClosestBinarySearchTreeValue {
+    public int closestValue(TreeNode root, double target) {
+        TreeNode next = target < root.val ? root.left : root.right;        
+        if (next == null)
+            return root.val;
+        
+        int closest = closestValue(next, target);       
+        
+        return Math.abs(root.val - target) < Math.abs(closest - target) ? root.val : closest;
+    }
+}
