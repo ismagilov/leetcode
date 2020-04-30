85e5ac78db0eff00b2c0eb48990ac4f1d95603b3
Ilya Ismagilov <ilyaisma@amazon.com>
2020-04-30 08:54:10 -0700

    Solved CountingElements in one pass

--- src/CountingElements.kt
+++ src/CountingElements.kt
@@ -10,10 +10,30 @@ class CountingElements {
 
         return result
     }
+
+    fun countElementsB(arr: IntArray): Int {
+        val hash = mutableMapOf<Int, Int>()
+
+        var result = 0
+        for (v in arr) {
+            hash[v + 1]?.let { result++ }
+
+            if (!hash.containsKey(v)) {
+                hash[v - 1]?.let { result += it }
+                hash[v] = 1
+            } else {
+                hash[v] = (hash[v] ?: 0) + 1
+            }
+        }
+
+        return result
+    }
 }
 
 fun main() {
-    println(CountingElements().countElementsA(intArrayOf(3, 2, 1)))
-    println(CountingElements().countElementsA(intArrayOf(1, 2, 3)))
-    println(CountingElements().countElementsA(intArrayOf(1, 1, 3, 3, 5, 5, 7, 7)))
+    println(CountingElements().countElementsB(intArrayOf(3, 2, 1)))
+    println(CountingElements().countElementsB(intArrayOf(1, 2, 3)))
+    println(CountingElements().countElementsB(intArrayOf(1, 1, 3, 3, 5, 5, 7, 7)))
+    println(CountingElements().countElementsB(intArrayOf(1, 1, 2, 2)))
+    println(CountingElements().countElementsB(intArrayOf(0, 4, 0, 2, 7, 2, 1, 7)))
 }
