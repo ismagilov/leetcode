dc2a891dc80f10dfccdc99bf92e97590ec9a2c64
Ilya Ismagilov <ismagilov@gmail.com>
2023-09-12 16:37:21 -0700

    Solve 1

--- src/TwoSum1.kt
+++ src/TwoSum1.kt
@@ -4,7 +4,7 @@ import java.lang.IllegalStateException
  * https://leetcode.com/problems/two-sum/
  */
 class TwoSum1 {
-    fun twoSum(nums: IntArray, target: Int): IntArray {
+    fun twoSumA(nums: IntArray, target: Int): IntArray {
         val map = mutableMapOf<Int, Int>()
         for (i in nums.indices) {
             val second = target - nums[i]
@@ -14,10 +14,28 @@ class TwoSum1 {
 
         throw IllegalStateException("Not possible")
     }
+
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val m = mutableMapOf<Int, Int>()
+        for ((i, v) in nums.withIndex()) {
+            val other = target - v
+            m[other]?.let { return intArrayOf(it, i) }
+            m[v] = i
+        }
+
+        return intArrayOf()
+    }
+
+    /*
+    [10, 5, -8, 9, 0]: 1
+    [10]
+    [10, -5]
+     */
 }
 
 fun main() {
     println(TwoSum1().twoSum(intArrayOf(2,7,11,15), 9).joinToString(separator = ", "))
     println(TwoSum1().twoSum(intArrayOf(2,19,-10,15), 9).joinToString(separator = ", "))
     println(TwoSum1().twoSum(intArrayOf(3,3), 6).joinToString(separator = ", "))
+    println(TwoSum1().twoSum(intArrayOf(10, 5, -8, 9, 0), 1).joinToString(separator = ", "))
 }
