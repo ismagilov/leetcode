86d1693c42134c024bcfcc38376809f004398386
Ilya Ismagilov <ismagilov@gmail.com>
2023-09-26 16:49:36 -0700

    Solve 22 using BFS

--- src/GenerateParentheses22.kt
+++ src/GenerateParentheses22.kt
@@ -1,7 +1,7 @@
 class GenerateParentheses22 {
     private var result = mutableListOf<String>()
 
-    fun generateParenthesis(n: Int): List<String> {
+    fun generateParenthesisA(n: Int): List<String> {
         generate("", 0, 0, n)
 
         return result
@@ -19,11 +19,38 @@ class GenerateParentheses22 {
         if (close < open)
             generate("$s)", open, close + 1, max)
     }
+
+    fun generateParenthesisB(n: Int): List<String> {
+        val result = mutableListOf<String>()
+
+        val q = ArrayDeque<Sequence>()
+
+        q.addLast(Sequence("", 0, 0))
+        while(q.isNotEmpty()) {
+            val head = q.removeFirst()
+
+            if (head.s.length == 2 * n) {
+                result.add(head.s)
+                continue;
+            }
+
+            if (head.open < n)
+                q.addLast(Sequence(head.s + "(", head.open + 1, head.close))
+
+            if (head.close < head.open)
+                q.addLast(Sequence(head.s + ")", head.open, head.close + 1))
+        }
+
+        return result
+    }
+
+    private data class Sequence(val s: String, val open: Int, val close: Int)
+
 }
 
 fun main() {
     val a = GenerateParentheses22()
 
-    println(a.generateParenthesis(2))
+    println(a.generateParenthesisB(2))
 }
 
