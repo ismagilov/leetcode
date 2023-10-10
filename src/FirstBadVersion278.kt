b9447ab9f2a4befe73c1cbf80286b185c62ae5e2
Ilya Ismagilov <ismagilov@gmail.com>
2023-10-10 15:58:51 -0700

    Solve 278 using binary search invariant based

---
+++ src/FirstBadVersion278.kt
@@ -0,0 +1,29 @@
+class FirstBadVersion278 {
+    fun firstBadVersion(n: Int) : Int {
+        var b = 0L
+        var e = n + 1L
+        while (b + 1 < e) {
+            val mid = b + (e - b) / 2L
+
+            when (isBadVersion(mid.toInt())) {
+                true -> {
+                    e = mid
+                }
+                false -> b = mid
+            }
+        }
+
+        return e.toInt()
+    }
+
+    private fun isBadVersion(version: Int): Boolean {
+        return version > 2147483600
+    }
+}
+
+fun main() {
+    val s = FirstBadVersion278()
+
+    println(s.firstBadVersion(11))
+    println(s.firstBadVersion(Int.MAX_VALUE))
+}
