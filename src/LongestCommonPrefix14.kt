48f5c378e5da0743ee4a55331aa38d3aa12d04b4
Ilya Ismagilov <ismagilov@gmail.com>
2023-10-13 11:12:37 -0700

    Solve 14 using bin search

--- src/LongestCommonPrefix14.kt
+++ src/LongestCommonPrefix14.kt
@@ -14,9 +14,32 @@ class LongestCommonPrefix14 {
     }
 
     fun longestCommonPrefixB(strs: Array<String>): String {
-        return ""
+        if (strs.isEmpty())
+            return ""
+
+        var l = -1
+        var r = strs[0].length
+        while (l + 1 < r) {
+            val mid = l + (r - l) / 2
+            if (isCommonPrefix(strs, strs[0].substring(0, mid + 1))) {
+                l = mid
+            } else {
+                r = mid
+            }
+        }
+
+        return strs[0].substring(0, l + 1)
+    }
+
+    private fun isCommonPrefix(strs: Array<String>, prefix: String): Boolean {
+        return strs.all { it.startsWith(prefix) }
     }
 }
 
 fun main() {
+    val s = LongestCommonPrefix14()
+    println(s.longestCommonPrefixB(arrayOf("a", "ab", "ac")))
+    println(s.longestCommonPrefixB(arrayOf("", "ab", "ac")))
+    println(s.longestCommonPrefixB(arrayOf("ab", "ab", "ab")))
+    println(s.longestCommonPrefixB(arrayOf("abc", "abe", "abg")))
 }
